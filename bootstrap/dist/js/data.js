190ad4e8777062dd6a14932e231ae6ed2464475a
Benny Chia <bienfantaisie@gmail.com>
2015-01-22 11:52:00 +0800

    a
    a

---
+++ bootstrap/dist/js/data.js
@@ -0,0 +1,105 @@
+$(function(){
+
+	// 定义cookie
+    var cookie = function(n, v, d) {
+        var k = '', D;
+        if (d) {
+            D = new Date();
+            D.setTime(D.getTime() + d * 24 * 60 * 60 * 1000);
+            k = '; expires=' + D.toGMTString();
+        }
+        document.cookie = n + '=' + v + k + '; path=/';
+    };
+
+    // 登录
+    // ------------------------------
+
+    // 获取登录控件
+    var login_btn = $('#login_btn'),
+        login_email = $('#login_email'),
+        login_password = $('#login_pass'),
+        login_form = $('#login_form'),
+        login_alert = $('#login_alert');
+
+    login_btn.on('click', function() {
+
+        // 获取控件输入的值
+        var data = {
+            admin_email: login_email.val(),
+            admin_password: login_password.val()
+        };
+
+        $.ajax({
+            url: 'http://218.241.220.36:8099/admin/json/AdminService/adminLogin',
+            type: 'post',
+            data: data,
+            dataType: 'json',
+            success: function(data) {
+            	if(data.result === 'SUCC'){
+
+                    // 注入cookie 名值时间
+                    cookie('x-session-key', data.session_key, 0.25);
+                    // 刷新页面
+
+                    login_form[0].action = './person-manage.html';
+                    login_form[0].submit();
+                    //location.href = location.origin + './person-manage.html';
+
+            	}else{
+            		login_alert.text(data.fail_text).show();
+            	}
+            }
+        });
+    });
+
+    // 登出
+    // ------------------------------
+
+    var logout = $('#logout');
+
+    logout.on('click',function(){
+
+        cookie('x-session-key',0,-1);
+
+    });
+
+    // 修改密码
+    // ------------------------------
+
+    var pwreset_btn = $('#pwreset_btn'),
+        pwreset_email = $('#pwreset_email'),
+        pwreset_oldpass = $('#pwreset_oldpass'),
+        pwreset_newpass = $('#pwreset_newpass'),
+        pwreset_repeatpass = $('#pwreset_repeatpass'),
+        pwreset_alert = $('#pwreset_alert');
+
+    pwreset_btn.on('click', function() {
+
+        // 获取控件输入的值
+        var data = {
+            admin_email: pwreset_email.val(),
+            old_password: pwreset_oldpass.val(),
+            new_password: pwreset_newpass.val()
+        };
+
+        $.ajax({
+            url: 'http://218.241.220.36:8099/admin/json/AdminService/adminResetPassword',
+            type: 'post',
+            data: data,
+            dataType: 'json',
+            success: function(data) {
+                console.log(data.result)
+                if(data.result === 'SUCC'){
+
+                    pwreset_alert.text('密码修改成功').show();
+
+                }else{
+                    pwreset_alert.text(data.fail_text).show();
+                }
+            }
+        });
+    });
+
+
+
+});
